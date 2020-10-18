e40a61514362937c2ab97caff11e0efd3ee069db
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2020-10-18 16:37:20 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceMtk_P2_096;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigMtk_P2_096
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
